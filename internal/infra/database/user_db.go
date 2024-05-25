9258c5128142421c817876fa8a59722a19cc8950
Ricardo Gonçalves <ricardo@conceitho.com>
2024-05-25 18:46:47 -0300

    feat: adiciona repositório para entidade User

---
+++ internal/infra/database/user_db.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"github.com/rgoncalvesrr/fullcycle-clean-arch/internal/entity"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	DB *gorm.DB
+}
+
+func NewUser(db *gorm.DB) *User {
+	return &User{DB: db}
+}
+
+func (u *User) Create(user *entity.User) error {
+	return u.DB.Create(user).Error
+}
+
+func (u *User) FindByEmail(email string) (*entity.User, error) {
+	var user *entity.User
+
+	err := u.DB.Where("email = ?", email).First(&user).Error
+
+	if err != nil {
+		user = nil
+	}
+
+	return user, err
+}
