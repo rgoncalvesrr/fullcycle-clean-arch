7476f653cad567a5aa8a0dc16dae308f0d4750f8
Ricardo Gonçalves <ricardo@conceitho.com>
2024-05-25 18:17:19 -0300

    test: adicona teste deunidade para entidade User

---
+++ internal/entity/user_test.go
@@ -0,0 +1,36 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	expectedName := "Ricardo"
+	expectedEMail := "ricardo@goncalves.biz"
+	expectedPassword := "123456"
+
+	u, e := NewUser(expectedName, expectedEMail, expectedPassword)
+
+	assert.Nil(t, e)
+	assert.NotNil(t, u)
+	assert.NotEmpty(t, u.ID)
+	assert.Equal(t, expectedName, u.Name)
+	assert.Equal(t, expectedEMail, u.EMail)
+	assert.NotEmpty(t, u.Password)
+}
+
+func TestUser_ValidatePassword(t *testing.T) {
+	expectedName := "Ricardo"
+	expectedEMail := "ricardo@goncalves.biz"
+	expectedPassword := "123456"
+
+	u, e := NewUser(expectedName, expectedEMail, expectedPassword)
+
+	assert.Nil(t, e)
+	assert.NotNil(t, u)
+	assert.True(t, u.ValidatePassword(expectedPassword))
+	assert.False(t, u.ValidatePassword("SenhaIncorreta"))
+	assert.NotEqual(t, expectedPassword, u.Password)
+}
