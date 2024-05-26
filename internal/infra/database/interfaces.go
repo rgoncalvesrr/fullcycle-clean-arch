09c5163ca83c07a181e9c178cd70ec7dd0936e33
Ricardo Gonçalves <ricardo@conceitho.com>
2024-05-26 19:30:23 -0300

    feat: adiciona interface de persistência de produtos

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -6,3 +6,11 @@ type UserInterface interface {
 	Create(user *entity.User) error
 	FindByEmail(email string) (*entity.User, error)
 }
+
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	FindAll(offset, limit int, sort string) ([]entity.Product, error)
+	FindByID(id string) (*entity.Product, error)
+	Update(*entity.Product) error
+	Delete(id string) error
+}
