ed59b3415248fee86de953e5b670dd77f8b5254c
Ricardo Gonçalves <ricardo@conceitho.com>
2024-05-26 18:26:15 -0300

    debug: versão para depuração

--- internal/infra/database/user_db.go
+++ internal/infra/database/user_db.go
@@ -5,26 +5,26 @@ import (
 	"gorm.io/gorm"
 )
 
-type User struct {
+type UserRepository struct {
 	DB *gorm.DB
 }
 
-func NewUser(db *gorm.DB) *User {
-	return &User{DB: db}
+func NewUser(db *gorm.DB) *UserRepository {
+	return &UserRepository{DB: db}
 }
 
-func (u *User) Create(user *entity.User) error {
+func (u *UserRepository) Create(user *entity.User) error {
 	return u.DB.Create(user).Error
 }
 
-func (u *User) FindByEmail(email string) (*entity.User, error) {
+func (u *UserRepository) FindByEmail(email string) (*entity.User, error) {
 	var user *entity.User
 
 	err := u.DB.Where("email = ?", email).First(&user).Error
 
 	if err != nil {
-		user = nil
+		return nil, err
 	}
 
-	return user, err
+	return user, nil
 }
--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -31,3 +31,28 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.EMail, userFound.EMail)
 	assert.NotEmpty(t, userFound.Password)
 }
+
+func TestFindByEmailUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("teste.db"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = db.AutoMigrate(&entity.User{})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	user, _ := entity.NewUser("Usuario X", "usuario@dominio.com", "12345")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.EMail)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.EMail, userFound.EMail)
+	assert.NotEmpty(t, userFound.Password)
+}
