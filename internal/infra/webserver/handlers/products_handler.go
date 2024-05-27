b5e5aa1c9fa55b93b41a44084b5deec1acb0d4cb
Ricardo Gonçalves <ricardo@conceitho.com>
2024-05-26 23:00:16 -0300

    feat: adiciona endpoint para obter um produto

--- internal/infra/webserver/handlers/products_handler.go
+++ internal/infra/webserver/handlers/products_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi"
 	"github.com/rgoncalvesrr/fullcycle-clean-arch/internal/dto"
 	"github.com/rgoncalvesrr/fullcycle-clean-arch/internal/entity"
 	"github.com/rgoncalvesrr/fullcycle-clean-arch/internal/infra/database"
@@ -48,6 +49,34 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 		CreatedAt: p.CreatedAt,
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(&o)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product, err := h.ProductGateway.FindByID(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	o := &dto.CreateProductOutput{
+		ID:        product.ID.String(),
+		Name:      product.Name,
+		Price:     product.Price,
+		CreatedAt: product.CreatedAt,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(&o)
 }
